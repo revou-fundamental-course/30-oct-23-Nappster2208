86c261ba8494c786f759e45d04c6fea7743f8a08
Nappster2208 <piamaulanaibrahim@gmail.com>
2023-11-04 20:06:36 +0700

    Rapihin tampilan sama nambahin event tampilan hasil - 2023-11-04

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById("btn_hitung").addEventListener("click", function () {  
+    // Show the result container and hide the form
+    document.getElementById("result_container").style.display = "block";
+    document.querySelector(".main-section").style.display = "none";
+})
